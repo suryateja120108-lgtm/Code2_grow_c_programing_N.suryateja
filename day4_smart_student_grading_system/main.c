6c2b9787dd51c5e7b882ec892a984b8e31b8d9db
suryateja120108-lgtm <suryateja120108@gmail.com>
2025-12-06 19:01:15 +0530

    Implement student grading system with attendance check
    Added student grading logic based on marks and attendance.

--- day4_smart_student_grading_system/main.c
+++ day4_smart_student_grading_system/main.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 int main()
 {
-int units;
-float bill,meter_charge=50;
-printf("Enter total units:");
-scanf("%d",&units);
-if (units<=100)
+float m1, m2, m3, m4, m5;
+float  attendance,total, percentage;
+char grade,remarks[20];
+printf("Enter marks in 5 subjects (out of 100): ");
+scanf("%f%f%f%f%f",&m1,&m2,&m3,&m4,&m5);
+printf("Enter attendance percentage:");
+scanf("%f",&attendance);
+if ((m1<0 || m1>100) || (m2<0 || m2>100) ||(m3<0 || m3>100) || (m4<0 || m4>100) ||(m5<0 || m5>100) || (attendance<0 || attendance>100))
 {
-bill=units*3.0;
+printf("\nError: Invalid input! Marks and attendance must be between 0 and 100.\n");
+return 0;
 }
-else if (units<=200)
+total=m1+m2+m3+m4+m5;
+percentage=total/5.00;
+if (attendance<75)
 {
-bill=units*4.5;
-}
-else if (units<=300)
-{
-bill=units*6.0;
+grade='F';
+printf(remarks,"Fail");
 }
 else
-{
-bill=units*8.0;
-}
-bill+=meter_charge;
-printf("Electricity Bill Summary");
-printf("Units Consumed:%d",units);
-printf("Total Bill including metercharge:%2f",bill);
-return 0;
-}
